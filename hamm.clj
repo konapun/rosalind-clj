c6a556c65da653ff3f1758600eb09a86911d9e75
konapun <bremen.braun@gmail.com>
2013-11-21 21:04:27 -0700

    learned about letfn

--- hamm.clj
+++ hamm.clj
@@ -1,9 +1,9 @@
 (defn hamming [seq1 seq2]	
-	(defn hamming-rec [seq1 seq2 diff]
-		(cond
-			(empty? seq1) diff
-			(empty? seq2) diff
-			:else (if (not= (first seq1) (first seq2))
-				(hamming-rec (rest seq1) (rest seq2) (inc diff))
-				(hamming-rec (rest seq1) (rest seq2) diff))))
-	(hamming-rec seq1 seq2 0))
+	(letfn [
+		(hamming-rec [seq1 seq2 diff]
+			(if (or (empty? seq1) (empty? seq2))
+				diff
+				(if (not= (first seq1) (first seq2))
+					(hamming-rec (rest seq1) (rest seq2) (inc diff))
+					(hamming-rec (rest seq1) (rest seq2) diff))))]
+			(hamming-rec seq1 seq2 0)))
